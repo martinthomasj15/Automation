09129a272411bf3e1fb22d006b1e825f5cde0d0f
Martin Thomas J <martinthomasj45@gmail.com>
2025-11-25 10:49:11 +0530

    Add files via upload

---
+++ Java_Learning/For_loop.java
@@ -0,0 +1,17 @@
+package Java_Learning;
+
+public class For_loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Print  number 1 to 10
+		for(int i=1; i<=10; i=i+1)
+		{
+	
+			System.out.println(i);
+		}
+
+	}
+
+}
